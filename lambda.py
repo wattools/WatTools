d29958222b263e5df68fd497fed9c129dfcd695d
Kyle Lexmond <github@kyl191.net>
2017-01-21 18:42:22 -0500

    Fix #38 - logging was improperly setup

--- lambda.py
+++ lambda.py
@@ -1,9 +1,13 @@
 import boto3
+import logging
 from urlparse import parse_qs
 from string import Template
 
 def handler(event, context):
     params = parse_qs(event['body'])
+    log = logging.getLogger()
+    log.setLevel(logging.DEBUG)
+    log.debug(params)
     msg = Template("""New message from $name ($email):
 $message""").safe_substitute(params)
     client = boto3.client('ses', region_name='us-west-2')

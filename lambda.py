b7a68e698ea188db56bfd56e227d18c6d3a9b667
Kyle Lexmond <sea.remote@kyl191.net>
2021-11-20 23:28:57 +0000

    Fix form submission lambda function to run py3.9

--- lambda.py
+++ lambda.py
@@ -1,42 +1,35 @@
+#!/usr/bin/env python3
 import boto3
 import logging
-from urlparse import parse_qs
-from string import Template
+import os
+from urllib.parse import parse_qs
+
 
 def handler(event, context):
     log = logging.getLogger()
-    log.setLevel(logging.DEBUG)
-    body = parse_qs(event['body'])
+    log.setLevel(logging.INFO)
+    body = parse_qs(event["body"])
     params = {
         "name": body.get("name", ["Null"])[0],
         "email": body.get("email", ["Null"])[0],
         "message": body.get("message", ["Null"])[0],
     }
-    log.debug(params)
-    msg = Template("""New message from $name ($email):
-$message""").safe_substitute(params)
-    client = boto3.client('ses', region_name='us-west-2')
+    log.info(f"Received params: {params}")
+    msg = f'New message from {params["name"]} ({params["email"]}): {params["message"]}'
+    client = boto3.client("ses", region_name=os.environ["AWS_REGION"])
+    dest_addr = os.environ["dest_addr"]
+    src_addr = os.environ["src_addr"]
     client.send_email(
-        Source='-source-',
+        Source=src_addr,
         Destination={
-            'ToAddresses': [
-                '-dest-',
+            "ToAddresses": [
+                dest_addr,
             ]
         },
         Message={
-            'Subject': {
-                'Data': 'Uwaterloo.xyz feedback'
-            },
-            'Body': {
-                'Text': {
-                    'Data': msg
-                }
-            }
-        }
+            "Subject": {"Data": "wattools feedback"},
+            "Body": {"Text": {"Data": msg}},
+        },
     )
     # Format as per https://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-set-up-simple-proxy.html#api-gateway-simple-proxy-for-lambda-output-format
-    return {
-        "statusCode": 200,
-        "headers": {},
-        "body": "All good!"
-    }
+    return {"statusCode": 200, "headers": {}, "body": {"res": "All good!"}}
